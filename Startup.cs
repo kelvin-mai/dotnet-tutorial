831d98d822ab877c1751bf8c57afc5e4ba1701b4
Kelvin Mai <kelvin.mai002@gmail.com>
2019-05-09 09:48:53 -0400

    Added mongodb bookstore

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TodoApi.Models;
+using TodoApi.Services;
 
 namespace TodoApi
 {
@@ -27,6 +28,7 @@ namespace TodoApi
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddScoped<BookService>();
       services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
